c7410464789f0397aa8b32c11a8552e1a940e0a0
vikash kumar <vicky.5sept@gmail.com>
2017-10-03 22:15:19 +0200

    Push Notification Added

--- POCDriverAppService/Controllers/TodoItemController.cs
+++ POCDriverAppService/Controllers/TodoItemController.cs
@@ -6,6 +6,10 @@ using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using POCDriverAppService.DataObjects;
 using POCDriverAppService.Models;
+// Vikash Notification Hub
+using System.Collections.Generic;
+using Microsoft.Azure.NotificationHubs;
+using Microsoft.Azure.Mobile.Server.Config;
 
 namespace POCDriverAppService.Controllers
 {
@@ -40,6 +44,41 @@ namespace POCDriverAppService.Controllers
         public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
         {
             TodoItem current = await InsertAsync(item);
+            // Notification Hub
+            // Get the settings for the server project.
+            HttpConfiguration config = this.Configuration;
+            MobileAppSettingsDictionary settings =
+                this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+            // Get the Notification Hubs credentials for the Mobile App.
+            string notificationHubName = settings.NotificationHubName;
+            string notificationHubConnection = settings
+                .Connections[MobileAppSettingsKeys.NotificationHubConnectionString].ConnectionString;
+
+            // Create a new Notification Hub client.
+            NotificationHubClient hub = NotificationHubClient
+            .CreateClientFromConnectionString(notificationHubConnection, notificationHubName);
+
+            // Sending the message so that all template registrations that contain "messageParam"
+            // will receive the notifications. This includes APNS, GCM, WNS, and MPNS template registrations.
+            Dictionary<string, string> templateParams = new Dictionary<string, string>();
+            templateParams["messageParam"] = item.Text + " was added to the list.";
+
+            try
+            {
+                // Send the push notification and log the results.
+                var result = await hub.SendTemplateNotificationAsync(templateParams);
+
+                // Write the success result to the logs.
+                config.Services.GetTraceWriter().Info(result.State.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                // Write the failure result to the logs.
+                config.Services.GetTraceWriter()
+                    .Error(ex.Message, null, "Push.SendAsync Error");
+            }
+
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
